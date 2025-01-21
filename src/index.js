304d9eb5371255c8fb088f00d162b57f614dd8ac
notgabie <gabriellamondvay@gmail.com>
2025-01-21 22:52:19 +0100

    feat: add user router to server

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ app.get('/', (req, res) => {
     res.send('Server is running');
 });
 
+app.use('/user', userRouter);
+
 app.listen(port, () => {
     console.log(`app is listening on port ${port}`);
 });
