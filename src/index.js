302d8bdd83c1d7e5799952aa9b2187ce487e179a
martinbannister <martin.bannister91@gmail.com>
2025-01-27 13:24:02 +0000

    fix(index.js): import userRouter

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 
 import 'dotenv/config';
 import express from 'express';
-import userRouter from './user/user.route';
+import userRouter from './user/user.route.js';
 const app = express();
 
 app.get('/', (req, res) => {
