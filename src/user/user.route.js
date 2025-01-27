a11a49ee636101be1f37dab69da655c3ba657e53
martinbannister <martin.bannister91@gmail.com>
2025-01-27 21:32:45 +0000

    fix(user.route.js): add express.json req body parser to userRouter

--- src/user/user.route.js
+++ src/user/user.route.js
@@ -2,6 +2,8 @@ import express from 'express';
 import { getUsers, createUser, getUserById, updateUser, deleteUser } from './user.controller.js';
 const userRouter = express.Router();
 
+userRouter.use(express.json());
+
 userRouter.get('/', getUsers);
 userRouter.post('/', createUser);
 // Routes that require id
